c9050e75397e5a8a71d9e38f66c702584c4cf037
yogutamilan <36201716+yogutamilan@users.noreply.github.com>
2018-03-20 16:44:16 +0530

    Create set-1-1.cpp

---
+++ set-1-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"Enter number";
+cin>>n;
+if(n=0)
+cout<<"positive";
+else
+if(n<0)
+cout<<"negative";
+else
+cout<<"zero";
+return 0;
+}
